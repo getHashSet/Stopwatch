206f60fb7f6564b8078e2115291a2425a97e5778
Matthew <matthew_carpenter@icloud.com>
2020-04-09 20:28:14 -0700

    Staged site with media queries and foundation HTML/CSS.

---
+++ app.js
@@ -0,0 +1,7 @@
+// Event listener to identify if DOM has completed loading.
+document.addEventListener("DOMContentLoaded", function(){
+    console.log('by Matthew Carpenter');
+    console.log('LinkedIn: https://linkedin.com/in/matthewcarpenter22');
+    console.log('GitHub: https://github.com/getHashSet');
+    console.log('Resume: https://prop7.herokuapp.com/matthew');
+  });
