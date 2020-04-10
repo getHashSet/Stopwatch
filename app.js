0d61e594fb0edd696d417166173ef1acd4af44d5
Matthew <matthew_carpenter@icloud.com>
2020-04-09 20:40:40 -0700

    linked javascript to html and tested via console log.

--- app.js
+++ app.js
@@ -1,7 +1,30 @@
 // Event listener to identify if DOM has completed loading.
-document.addEventListener("DOMContentLoaded", function(){
-    console.log('by Matthew Carpenter');
-    console.log('LinkedIn: https://linkedin.com/in/matthewcarpenter22');
-    console.log('GitHub: https://github.com/getHashSet');
-    console.log('Resume: https://prop7.herokuapp.com/matthew');
+document.addEventListener("DOMContentLoaded", function () {
+  console.log("by Matthew Carpenter");
+  console.log("LinkedIn: https://linkedin.com/in/matthewcarpenter22");
+  console.log("GitHub: https://github.com/getHashSet");
+  console.log("Resume: https://prop7.herokuapp.com/matthew");
+
+  // ========================== //
+  // === OBJECT CONSTRUCTOR === //
+  // ========================== //
+
+  // ================= //
+  // === FUNCTIONS === //
+  // ================= //
+  function addField(el) {
+    console.log(el);
+  };
+
+  // ======================= //
+  // === EVENT LISTENERS === //
+  // ======================= //
+  document.addEventListener("click", function (event) {
+    if (
+      event.target.attributes.name !== null &&
+      event.target.attributes.name !== undefined
+    ) {
+      addField(event.target);
+    }
   });
+});
