80efdd81a54cd32c2718fe30391381f753d01bed
Matthew <matthew_carpenter@icloud.com>
2020-04-09 21:00:10 -0700

    selected css for desktop and flex for mobile to reduce media queries.

--- app.js
+++ app.js
@@ -13,7 +13,13 @@ document.addEventListener("DOMContentLoaded", function () {
   // === FUNCTIONS === //
   // ================= //
   function addField(el) {
-    console.log(el);
+    // console.log(el); // log event to make sure the correct element was selected.
+    
+    // 1. Assign the value of the upload field location to a variable.
+    const uploadField = document.getElementsByClassName('uploadfield');
+
+    // 3. Upload a new stopwatch skeleton to the DOM using the uploadField's innerHTML as the destination. 
+    uploadField[0].innerHTML = uploadField[0].innerHTML + `<div class="stopwatch"></div>`;
   };
 
   // ======================= //
